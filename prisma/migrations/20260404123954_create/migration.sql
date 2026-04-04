2e0910323f2c23fdbdfec328ced6b5bea1b5f82c
Michel John <michel.santos@dataside.com.br>
2026-04-04 09:49:36 -0300

    feat: add Category model and migration for categories table

---
+++ prisma/migrations/20260404123954_create/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "category" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "category" ADD CONSTRAINT "category_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
