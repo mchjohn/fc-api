d35b5fdc203c592e159a20c78c8c2c406fdfc2c7
Michel John <michel.santos@dataside.com.br>
2026-04-06 18:00:28 -0300

    feat: integrate PrismaService into UsersModule and enhance UsersService for user creation with email conflict handling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException({ message: 'Este email já está em uso.' });
+    }
+
+    const user = this.prismaService.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+    return user;
   }
 
   remove(id: number) {
