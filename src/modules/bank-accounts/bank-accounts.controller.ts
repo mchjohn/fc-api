5b190a04e52e3e0eba2aba25019ace84f91fae15
Michel John <michel.santos@dataside.com.br>
2026-05-02 15:52:41 -0300

    feat: update BankAccounts module to handle user-specific bank account retrieval and updates

--- src/modules/bank-accounts/bank-accounts.controller.ts
+++ src/modules/bank-accounts/bank-accounts.controller.ts
@@ -30,17 +30,25 @@ export class BankAccountsController {
     return this.bankAccountsService.findAllByUserId(userId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bankAccountsService.findOne(+id);
+  @Get(':bankAccountId')
+  findOne(
+    @ActiveUserId() userId: string,
+    @Param('bankAccountId') bankAccountId: string,
+  ) {
+    return this.bankAccountsService.findFirst(userId, bankAccountId);
   }
 
-  @Put(':id')
+  @Put(':bankAccountId')
   update(
-    @Param('id') id: string,
+    @ActiveUserId() userId: string,
+    @Param('bankAccountId') bankAccountId: string,
     @Body() updateBankAccountDto: UpdateBankAccountDto,
   ) {
-    return this.bankAccountsService.update(+id, updateBankAccountDto);
+    return this.bankAccountsService.update(
+      userId,
+      bankAccountId,
+      updateBankAccountDto,
+    );
   }
 
   @Delete(':id')
--- src/modules/bank-accounts/bank-accounts.service.ts
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
@@ -26,12 +26,42 @@ export class BankAccountsService {
     return this.bankAccountsRepo.findMany({ where: { userId } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} bankAccount`;
+  async findFirst(userId: string, bankAccountId: string) {
+    const bankAccount = await this.bankAccountsRepo.findFirst({
+      where: { id: bankAccountId, userId },
+    });
+
+    if (!bankAccount) {
+      throw new NotFoundException('Bank account not found.');
+    }
+
+    return bankAccount;
   }
 
-  update(id: number, updateBankAccountDto: UpdateBankAccountDto) {
-    return `This action updates a #${id} bankAccount`;
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    const { name, initialBalance, type, color } = updateBankAccountDto;
+
+    const isOwner = await this.bankAccountsRepo.findFirst({
+      where: { id: bankAccountId, userId },
+    });
+
+    if (!isOwner) {
+      throw new NotFoundException('Bank account not found.');
+    }
+
+    return this.bankAccountsRepo.update({
+      where: { id: bankAccountId },
+      data: {
+        name,
+        initialBalance,
+        type,
+        color,
+      },
+    });
   }
 
   remove(id: number) {
