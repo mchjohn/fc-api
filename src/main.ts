dab4c560b0b6cb7c6a632165ac2e73a79537255b
Michel John <michel.john@hotmail.com>
2026-05-04 19:41:29 -0300

    feat: add CORS support to the application

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
